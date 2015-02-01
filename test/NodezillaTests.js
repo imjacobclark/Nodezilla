57bc6c834ac2478235e9b9a2b810b169ee9cfd2f
Jacob Clark <jacob.jh.clark@googlemail.com>
2015-02-01 16:01:03 +0000

    Implements initial pending test

---
+++ test/NodezillaTests.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('Nodezilla', function(){
+  describe('createClients', function(){
+    xit('should call recursiveRequest() 10 times', function(){
+
+    });
+  });
+});
